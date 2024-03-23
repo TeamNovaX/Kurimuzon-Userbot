950c73286ca162587e9ca2036c4b5d80219750e3
Yash Kedar <122774834+NyaNekko@users.noreply.github.com>
2024-03-23 14:56:35 +0530

    Update autonaruto.py

--- plugins/autonaruto.py
+++ plugins/autonaruto.py
@@ -10,7 +10,7 @@ from utils.misc import modules_help
     ~filters.scheduled & filters.text & filters.me & ~filters.forwarded
 )
 async def naauto(client: Client, message: Message):
-    if not message.from_user.id = 5416991774:
+    if not message.from_user.id == 5416991774:
         return
     if "has challanged you!" in message:
         await message.click(0)
