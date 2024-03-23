520918b5c2760eb73b2c37dcd67890ff27ce282b
Yash Kedar <122774834+NyaNekko@users.noreply.github.com>
2024-03-23 14:53:53 +0530

    Update autonaruto.py

--- plugins/autonaruto.py
+++ plugins/autonaruto.py
@@ -7,11 +7,13 @@ from utils.misc import modules_help
 
 
 @Client.on_message(
-    ~filters.scheduled & command(["na", "explore"]) & filters.me & ~filters.forwarded
+    ~filters.scheduled & filters.text & filters.me & ~filters.forwarded
 )
 async def naauto(client: Client, message: Message):
-    await message.delete()
-    a = await message.reply("/explore")
+    if not message.from_user.id = 5416991774:
+        return
+    if "has challanged you!" in message:
+        await message.click(0)
 
 
 module = modules_help.add_module("autonaruto", __file__)
